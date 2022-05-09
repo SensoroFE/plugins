4f1ab2fc114c788bc90a1a26ff0f650c481ea4c7
wangxingkang <wang_xingkang@qq.com>
2022-05-10 01:46:05 +0800

    ✨ feat: 实现样式自动引入

--- packages/sensoro-design-react/src/index.tsx
+++ packages/sensoro-design-react/src/index.tsx
@@ -4,16 +4,18 @@ import { resolveProjectDep } from './utils/resolveProjectDep';
 
 export default (api: IApi) => {
   let pkgPath: string;
+
   try {
     pkgPath =
       resolveProjectDep({
         pkg: api.pkg,
         cwd: api.cwd,
-        dep: 'antd',
-      }) || dirname(require.resolve('antd/package.json'));
+        dep: '@sensoro-design/react',
+      }) || dirname(require.resolve('@sensoro-design/react/package.json'));
   } catch (e) {}
 
   api.describe({
+    key: 'sensoroDesign',
     config: {
       schema(Joi) {
         return Joi.object({
@@ -27,9 +29,28 @@ export default (api: IApi) => {
 
   function checkPkgPath() {
     if (!pkgPath) {
-      throw new Error(`Can't find antd package. Please install antd first.`);
+      throw new Error(`Can't find @sensoro-design/react package. Please install @sensoro-design/react first.`);
     }
   }
+
+  api.modifyAppData((memo) => {
+    checkPkgPath();
+    const version = require(`${pkgPath}/package.json`).version;
+    memo.sensoroDesign = {
+      pkgPath,
+      version,
+    };
+    return memo;
+  });
+
+  api.modifyConfig((memo) => {
+    checkPkgPath();
+
+    // @sensoro-design/react import
+    memo.alias['@sensoro-design/react'] = pkgPath;
+
+    return memo;
+  });
 
   // babel-plugin-import
   api.addExtraBabelPlugins(() => {
